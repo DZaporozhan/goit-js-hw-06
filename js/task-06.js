7ef7181a36f9c490683d17a7a9b553da8fd6814d
Denis Zaporozhan <zaporozhanbiz@gmail.com>
2022-07-25 11:46:56 +0300

    Завдання 6

---
+++ js/task-06.js
@@ -0,0 +1,14 @@
+const refs = {
+  inputElem: document.querySelector("#validation-input"),
+};
+const valid = Number(refs.inputElem.dataset.length);
+
+refs.inputElem.addEventListener("input", (event) => {
+  if (event.currentTarget.value.length !== valid) {
+    refs.inputElem.classList.add("invalid");
+    refs.inputElem.classList.remove("valid");
+  } else {
+    refs.inputElem.classList.add("valid");
+    refs.inputElem.classList.remove("invalid");
+  }
+});
