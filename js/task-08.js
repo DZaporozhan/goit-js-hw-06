2b43e5884588fd21c1106e2e900cd33910eb1128
Denis Zaporozhan <zaporozhanbiz@gmail.com>
2022-07-25 13:01:53 +0300

    Завдання 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,18 @@
+const refs = {
+  form: document.querySelector(".login-form"),
+};
 
+const onSubmit = (e) => {
+  e.preventDefault();
+  const {
+    email: { value: emailValue },
+    password: { value: passwordValue },
+  } = e.target.elements;
+  const messageAlert = "Усі поля мають бути заповнені";
+  emailValue === "" || passwordValue === ""
+    ? alert(messageAlert)
+    : console.log({ emailValue, passwordValue });
+  refs.form.reset();
+};
+
+refs.form.addEventListener("submit", onSubmit);
