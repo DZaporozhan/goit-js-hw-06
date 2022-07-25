9c00f0919065f5454d4af5cab2c543ebe0600f0b
Denis Zaporozhan <zaporozhanbiz@gmail.com>
2022-07-25 13:14:58 +0300

    Завдання 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const refs = {
+  body: document.querySelector("body"),
+  colorChangeBtn: document.querySelector(".change-color"),
+  colorValueText: document.querySelector(".color"),
+};
+const onColorChangClick = (e) => {
+  refs.body.style.backgroundColor = getRandomHexColor();
+  refs.colorValueText.innerHTML = getRandomHexColor();
+};
+refs.colorChangeBtn.addEventListener("click", onColorChangClick);
